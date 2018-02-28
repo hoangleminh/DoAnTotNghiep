a5e162f8933f486213a96b57ecf4ea0cfa73c076
hoangleminh <leminhhoang1960@gmail.com>
2018-02-28 21:11:54 +0700

    thay file moi

---
+++ Tanker2D/Assets/Scripts/UI/SoundController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void PlayEffect(AudioClip effectsound)
+    {
+        GameObject playEffectObj = new GameObject();
+        playEffectObj.AddComponent<AudioSource>();
+        AudioSource audiosource = playEffectObj.GetComponent<AudioSource>();
+        audiosource.PlayOneShot(effectsound);
+        Destroy(playEffectObj, effectsound.length);
+    }
+}
---
+++ Tanker2D/Assets/Scripts/UI/UIController.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIController : MonoBehaviour {
+
+    public static UIController _instance;
+    public static UIController instance
+    {
+        get
+        {
+            return _instance;
+        }
+    }
+
+    void Awake()
+    {
+        if (_instance != null)
+        {
+            Destroy(this.gameObject);
+        }
+        _instance = this;
+    }
+
+    public StartUI startUI;
+    public PauseUI pauseUI;
+    public GameoverUI gameoverUI;
+    public GameObject GameControl;
+    public bool isPause;
+
+    // Use this for initialization
+    void Start()
+    {
+        ShowStartUI();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void ShowStartUI()
+    {
+        startUI.gameObject.SetActive(true);
+        pauseUI.gameObject.SetActive(false);
+        gameoverUI.gameObject.SetActive(false);
+        GameControl.gameObject.SetActive(false);
+    }
+
+    public void ShowPauseUI()
+    {
+        startUI.gameObject.SetActive(false);
+        pauseUI.gameObject.SetActive(true);
+        gameoverUI.gameObject.SetActive(false);
+        GameControl.gameObject.SetActive(false);
+    }
+
+    public void ShowGameoverUI()
+    {
+        startUI.gameObject.SetActive(false);
+        pauseUI.gameObject.SetActive(false);
+        gameoverUI.gameObject.SetActive(true);
+        GameControl.gameObject.SetActive(false);
+    }
+
+    public void ShowGameplay()
+    {
+        startUI.gameObject.SetActive(false);
+        pauseUI.gameObject.SetActive(false);
+        gameoverUI.gameObject.SetActive(false);
+        GameControl.gameObject.SetActive(true);
+    }
+}
